1f12c45f5618bb299203eab0658a9b1b317c544e
Pablo Raul Montenegro Ph.D. | MCs. <pablo.r.monte@gmail.com>
2025-04-21 14:15:21 -0300

    f

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const F_API_URL = import.meta.env.VITE_F_API_URL;
 export const FDEV_API_URL = import.meta.env.VITE_FDEV_API_URL;
-export const VITE_FRONT = import.meta.env.SITE_PROD;
+export const VITE_FRONT = import.meta.env.VITE_SITE_PROD;
