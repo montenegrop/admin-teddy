6c9c9adced27fcd8cd30541af3a3ca7997bb3fb4
Pablo Raul Montenegro Ph.D. | MCs. <pablo.r.monte@gmail.com>
2025-06-03 10:06:03 -0300

    f

--- src/hooks/useCalls.ts
+++ src/hooks/useCalls.ts
@@ -3,7 +3,7 @@ import { UserService } from "@/services/api";
 
 export function useCalls() {
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["calls"],
     queryFn: UserService.getCalls,
     staleTime: 1000 * 60 * 1,
   });
--- src/hooks/useCompanies.ts
+++ src/hooks/useCompanies.ts
@@ -3,7 +3,7 @@ import { UserService } from "@/services/api";
 
 export function useCompanies() {
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["companies"],
     queryFn: UserService.getCompanies,
     staleTime: 1000 * 60 * 1,
   });
